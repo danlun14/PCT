cf139555e7f6de5d09bb183915fd6097f0732ff7
Данила Лунев <Danila@localhost.localdomain>
2021-04-11 21:18:41 +0700

    da

---
+++ lab2/main.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hpctimer.h"
+#include <omp.h>
+
+enum
+{
+	N = 25000,
+	NREPS = 1
+};
+
+void printVector(double *v, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%.3lf ", v[i]);
+	}
+	printf("\n");
+}
+
+/*int main()
+{
+#pragma omp parallel
+	{
+		printf("Hey, thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+	}
+
+	return 0;
+}
+*/
+void run_parallel()
+{
+	int m = N, n = N;
+	double *a, *b, *c;
+	// Allocate memory for 2-d array a[m, n]
+	a = malloc(sizeof(*a) * m * n);
+	b = malloc(sizeof(*b) * n);
+	c = malloc(sizeof(*c) * m);
+#pragma omp parallel
+	{
+		int nthreads = omp_get_num_threads();
+		int threadid = omp_get_thread_num();
+		int items_per_thread = m / nthreads;
+		int lb = threadid * items_per_thread;
+		int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+		for (int i = lb; i <= ub; i++)
+		{
+			for (int j = 0; j < n; j++)
+				a[i * n + j] = i + j;
+			c[i] = 0.0;
+		}
+#pragma omp barrier
+	}
+	for (int j = 0; j < n; j++)
+	{
+		b[j] = j;
+	}
+
+#pragma omp parallel
+	{
+		int nthreads = omp_get_num_threads();
+		int threadid = omp_get_thread_num();
+		int items_per_thread = m / nthreads;
+		int lb = threadid * items_per_thread;
+		int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+		for (int i = lb; i <= ub; i++)
+		{
+			for (int j = 0; j < n; j++)
+			
+				// 4 обращения к памяти: Load c[i], Load a[i][j], Load b[j], Store c[i]
+				// 2 арифметические операции + и *
+				c[i] = c[i] + a[i * n + j] * b[j];
+		}
+	}
+}
+
+int main()
+{
+	int m = N, n = N;
+	double *a, *b, *c;
+	// Allocate memory for 2-d array a[m, n]
+	a = malloc(sizeof(*a) * m * n);
+	b = malloc(sizeof(*b) * n);
+	c = malloc(sizeof(*c) * m);
+#pragma omp parallel num_threads(6)
+	{
+		int nthreads = omp_get_num_threads();
+		int threadid = omp_get_thread_num();
+		int items_per_thread = m / nthreads;
+		int lb = threadid * items_per_thread;
+		int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+		for (int i = lb; i <= ub; i++)
+		{
+			for (int j = 0; j < n; j++)
+				a[i * n + j] = i + j;
+			c[i] = 0.0;
+		}
+#pragma omp barrier
+	}
+	for (int j = 0; j < n; j++)
+	{
+		b[j] = j;
+	}
+
+	printf("\nN = %d\n", N);
+	double t = hpctimer_getwtime();
+	for (int i = 0; i < NREPS; i++)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			c[i] = 0.0;
+			for (int j = 0; j < n; j++)
+				c[i] += a[i * n + j] * b[j];
+		}
+	}
+	printVector(c, 5);
+	t = hpctimer_getwtime() - t;
+	t = t / NREPS;
+	printf("Elapsed time non parallel: %.6f sec.\n", t);
+
+	double t1 = omp_get_wtime();
+#pragma omp parallel num_threads(6)
+	{
+#pragma omp master
+		printf("Num Threads = %d\n", omp_get_num_threads());
+
+		int nthreads = omp_get_num_threads();
+		int threadid = omp_get_thread_num();
+		int items_per_thread = N / nthreads;
+		int lb = threadid * items_per_thread;
+		int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+		for (int i = lb; i <= ub; i++)
+		{
+			c[i] = 0.0;
+			for (int j = 0; j < n; j++)
+				c[i] += a[i * n + j] * b[j];
+		}
+
+#pragma omp barrier
+	}
+	printVector(c, 5);
+	t1 = omp_get_wtime() - t1;
+	t1 = t1 / NREPS;
+
+	printf("Elapsed time: %.6f sec.\n", t1);
+	printf("Speedup = %.4lf\n", t / t1);
+	return 0;
+}
